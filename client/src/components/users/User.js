ec1a7520560570961763a0cddfd12189d53bb07c
danishakh <danishakh@gmail.com>
2019-08-08 23:38:26 -0400

    added react frontend and set up a basic component to pull data from express. added start scripts to client package.json. added dev script to server package.json

---
+++ client/src/components/users/User.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import './User.css';
+
+class User extends Component {
+    constructor() {
+        super();
+
+        this.state = {
+            users: []
+        }
+    }
+
+    componentDidMount() {
+        fetch('/api/users')
+            .then(res => res.json())
+            .then(users => this.setState({users}, () => console.log('Users fetched...', users)));
+    }
+
+
+    render() {
+        return (
+            <div className='users-list'>
+                <h2>Users</h2>
+                <ul>
+                    {this.state.users.map(user => 
+                        <li key={user.id}>{user.firstname} {user.lastname}</li>
+                    )}
+                </ul>
+            </div>
+        )
+    }
+}
+
+export default User;
